4f5698c506d9198ac3d0e35f2d342f523cfef550
RootinTootinVibes <cullenmoore417@gmail.com>
2025-04-06 13:51:32 -0400

    button change

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import classes from './Button.module.css';
 
 const Button = ({label, onClick}) => {
-    return <button className={classes.button} type={onClick}>{label}</button>
+    return <button className={classes.button} onClick={onClick}>{label}</button>
 };
 
 export default Button;
