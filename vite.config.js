0316d4495bcd1a3eeea8d35fccade7f04b0e30f7
RootinTootinVibes <cullenmoore417@gmail.com>
2025-04-05 21:50:43 -0400

    n

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/rootintootinvibes.github.io/final-project/'
+  base: '/final-project/'
 })
